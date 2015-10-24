0d4b015b1d1a8b11b4d2fa21c50d6f880ad7f126
caeruleus1F <garrett.bates@outlook.com>
2015-10-24 10:12:46 -0700

    initial commit

---
+++ cs_EVE_Server_Status/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace cs_EVE_Server_Status
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbStatus = new System.Windows.Forms.TextBox();
+            this.txbPlayers = new System.Windows.Forms.TextBox();
+            this.txbSinceLast = new System.Windows.Forms.TextBox();
+            this.txb30ST = new System.Windows.Forms.TextBox();
+            this.txbNext = new System.Windows.Forms.TextBox();
+            this.timerRequest = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(68, 142);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 0;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Players:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Since Last:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "30-ST:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Next:";
+            // 
+            // txbStatus
+            // 
+            this.txbStatus.Location = new System.Drawing.Point(78, 12);
+            this.txbStatus.Name = "txbStatus";
+            this.txbStatus.ReadOnly = true;
+            this.txbStatus.Size = new System.Drawing.Size(100, 20);
+            this.txbStatus.TabIndex = 7;
+            // 
+            // txbPlayers
+            // 
+            this.txbPlayers.Location = new System.Drawing.Point(78, 38);
+            this.txbPlayers.Name = "txbPlayers";
+            this.txbPlayers.ReadOnly = true;
+            this.txbPlayers.Size = new System.Drawing.Size(100, 20);
+            this.txbPlayers.TabIndex = 8;
+            // 
+            // txbSinceLast
+            // 
+            this.txbSinceLast.Location = new System.Drawing.Point(78, 64);
+            this.txbSinceLast.Name = "txbSinceLast";
+            this.txbSinceLast.ReadOnly = true;
+            this.txbSinceLast.Size = new System.Drawing.Size(100, 20);
+            this.txbSinceLast.TabIndex = 9;
+            // 
+            // txb30ST
+            // 
+            this.txb30ST.Location = new System.Drawing.Point(78, 90);
+            this.txb30ST.Name = "txb30ST";
+            this.txb30ST.ReadOnly = true;
+            this.txb30ST.Size = new System.Drawing.Size(100, 20);
+            this.txb30ST.TabIndex = 10;
+            // 
+            // txbNext
+            // 
+            this.txbNext.Location = new System.Drawing.Point(78, 116);
+            this.txbNext.Name = "txbNext";
+            this.txbNext.ReadOnly = true;
+            this.txbNext.Size = new System.Drawing.Size(100, 20);
+            this.txbNext.TabIndex = 11;
+            // 
+            // timerRequest
+            // 
+            this.timerRequest.Interval = 1000;
+            this.timerRequest.Tick += new System.EventHandler(this.timerRequest_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 174);
+            this.Controls.Add(this.txbNext);
+            this.Controls.Add(this.txb30ST);
+            this.Controls.Add(this.txbSinceLast);
+            this.Controls.Add(this.txbPlayers);
+            this.Controls.Add(this.txbStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnQuery);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Server Status";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbStatus;
+        private System.Windows.Forms.TextBox txbPlayers;
+        private System.Windows.Forms.TextBox txbSinceLast;
+        private System.Windows.Forms.TextBox txb30ST;
+        private System.Windows.Forms.TextBox txbNext;
+        private System.Windows.Forms.Timer timerRequest;
+    }
+}
+
---
+++ cs_EVE_Server_Status/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+namespace cs_EVE_Server_Status
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument _xmldoc = new XmlDocument();
+        bool _server_online = false;
+        int _online_players = 0;
+        int _last_player_count = 0;
+        DateTime _cached_until;
+        DateTime _current_time;
+        WebClient _web = new WebClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            _web.DownloadStringCompleted += ProcessServerResponse;
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private void GetData()
+        {
+
+            try
+            {
+                _web.DownloadStringAsync(new Uri("https://api.eveonline.com/server/ServerStatus.xml.aspx/"));
+            }
+            catch (Exception ex)
+            {
+                txbPlayers.Text = "N/A";
+                txbStatus.Text = "N/A";
+            }
+        }
+
+        void ProcessServerResponse(object sender, DownloadStringCompletedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            _last_player_count = _online_players;
+            _online_players = 0;
+            int player_difference = 0;
+
+            try
+            {
+                _xmldoc.LoadXml(e.Result);
+                _current_time = DateTime.Parse(_xmldoc.SelectSingleNode("/eveapi/currentTime").InnerText);
+                _cached_until = DateTime.Parse(_xmldoc.SelectSingleNode("/eveapi/cachedUntil").InnerText);
+                _server_online = Convert.ToBoolean(_xmldoc.SelectSingleNode("/eveapi/result/serverOpen").InnerText);
+
+                if (_server_online)
+                {
+                    // Status
+                    txbStatus.Text = "Online";
+                    txbStatus.BackColor = Color.LawnGreen;
+
+                    // Players
+                    _online_players = Convert.ToInt32(_xmldoc.SelectSingleNode("/eveapi/result/onlinePlayers").InnerText);
+                    txbPlayers.Text = _online_players.ToString();
+
+                    // Since Last
+                    player_difference = _online_players - _last_player_count;
+                    float pct_change = 100 * (float)_online_players / _last_player_count;
+                    sb.Append(player_difference).Append(" ")
+                        .Append(pct_change.ToString("N1"))
+                        .Append("%");
+                    txbSinceLast.Text = sb.ToString();
+                    sb.Clear();
+
+                    // 30-ST
+                    DetermineRelativePlayerPopulation(player_difference);
+
+                    // Next
+                    txbNext.Text = _cached_until.ToLocalTime().AddSeconds(5).ToLongTimeString();
+
+                    // Timer
+                    int interval = Convert.ToInt32(_cached_until.Subtract(_current_time).TotalMilliseconds) + 5000;
+                    timerRequest.Interval = interval;
+                    timerRequest.Enabled = true;
+
+                    // Write to ccp_server.csv
+                    using (StreamWriter w = new StreamWriter("ccp_server.csv", true))
+                    {
+                        sb.Append(DateTime.Now.ToShortDateString())
+                            .Append(" ")
+                            .Append(DateTime.Now.ToLongTimeString())
+                            .Append(",")
+                            .Append(_online_players);
+                        w.WriteLine(sb.ToString());
+                        w.Close();
+                        sb.Clear();
+                    }
+                }
+                else
+                {
+                    txbStatus.Text = "Offline";
+                    txbStatus.BackColor = Color.Red;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                txbStatus.Text = "Comm. Error";
+            }
+        }
+
+        private void DetermineRelativePlayerPopulation(int player_difference)
+        {
+            List<string> lines = new List<string>();
+            string line = null;
+            DateTime now = DateTime.Now;
+            DateTime lineDate;
+            StringBuilder sb = new StringBuilder();
+            int qualifying_player_total = 0;
+            int average_now_player_count = 0;
+            int difference_30_ST = 0;
+
+            using (StreamReader r = new StreamReader("ccp_server.csv"))
+            {
+                r.ReadLine(); // discard first line, header
+
+                while (!r.EndOfStream)
+                {
+                    line = r.ReadLine();
+                    lineDate = DateTime.Parse(line.Split(',')[0]);
+
+                    if (now.AddDays(-30) <= lineDate &&
+                        now.DayOfWeek == lineDate.DayOfWeek &&
+                        now.TimeOfDay.Add(TimeSpan.FromMinutes(-5)) <= lineDate.TimeOfDay &&
+                        now.TimeOfDay.Add(TimeSpan.FromMinutes(5)) >= lineDate.TimeOfDay)
+                    {
+                        lines.Add(line);
+                    }
+                }
+
+                r.Close();
+            }
+
+            foreach (string item in lines)
+            {
+                qualifying_player_total += Convert.ToInt32(item.Split(',')[1]);
+            }
+
+            average_now_player_count = Convert.ToInt32(qualifying_player_total / lines.Count);
+            difference_30_ST = _online_players - average_now_player_count;
+            float pct_difference = 100 * (float)_online_players / average_now_player_count;
+            sb.Append(difference_30_ST)
+                .Append(" ")
+                .Append(pct_difference.ToString("N1"))
+                .Append("%");
+            txb30ST.Text = sb.ToString();
+
+            sb.Clear();
+            lines.Clear();
+        }
+
+        private void timerRequest_Tick(object sender, EventArgs e)
+        {
+            GetData();
+        }
+    }
+}
